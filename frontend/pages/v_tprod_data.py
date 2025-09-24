e0b022269e7c3bc8feb66c3be0afb8f86f51d8ee
PhilippeAbadie <philippe@evolveconsultores.com>
2025-09-23 23:16:17 -0600

    fix despliega la tabla correctamente [1.0.130.4]

--- frontend/pages/v_tprod_data.py
+++ frontend/pages/v_tprod_data.py
@@ -18,15 +18,13 @@ def v_tblprod_data_page():
 
         # 3. Definir columnas
         columnas = [
-            {"name": "proveedor", "label": "Proveedor", "field": "proveedor", "align": "left"},
-            {"name": "familia", "label": "Familia", "field": "familia", "align": "left"},
-            {"name": "sku_prov", "label": "SKU Proveedor", "field": "sku_prov", "align": "left"},
-            {"name": "sku_sys", "label": "SKU Sistema", "field": "sku_sys", "align": "left"},
-            {"name": "nombre", "label": "Nombre Producto", "field": "nombre", "align": "left"},
-            {"name": "precio_lista", "label": "Precio Lista", "field": "precio_lista", "align": "right"},
-            {"name": "version", "label": "Versión", "field": "version", "align": "center"},
-            {"name": "fecha_update", "label": "Última Actualización", "field": "fecha_update", "align": "center"},
-            {"name": "usuario_update", "label": "Actualizado por", "field": "usuario_update", "align": "left"},
+            {"name": "familia", "label": "Familia", "field": "familia", "align": "left", "sortable": True},
+            {"name": "proveedor", "label": "Proveedor", "field": "proveedor", "align": "left", "sortable": True},
+            {"name": "sku_prov", "label": "SKU Proveedor", "field": "sku_prov", "align": "center", "sortable": True},
+            {"name": "sku_sys", "label": "SKU Sistema", "field": "sku_sys", "align": "center", "sortable": True},
+            {"name": "nombre", "label": "Nombre Producto", "field": "nombre", "align": "left", "sortable": True},
+            {"name": "version", "label": "Versión", "field": "version", "align": "center", "sortable": True},
+            {"name": "fecha_update", "label": "Última Actualización", "field": "fecha_update", "align": "center", "sortable": True},
         ]
 
 
